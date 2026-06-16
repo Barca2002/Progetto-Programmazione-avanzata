e069974dfb98f4596491caa937631d62f20f8871
Operatore∇ <54327323+SpectreCoded@users.noreply.github.com>
2026-06-16 12:09:19 +0200

    Implementato ErrorFactory, modello AppError, enumerativo degli sttus code e relativi messaggi (DA RIVEDERE BENE). Implementazione parziale della rotta di login.

---
+++ backend/src/models/AppErrorModel.ts
@@ -0,0 +1,13 @@
+import { AppErrorName } from "../enums/StatusMessages";
+
+export class AppError extends Error {
+    constructor(
+        public readonly statusCode: number,
+        public readonly statusCodeString: AppErrorName,  // stringa leggibile invece di un numero
+        message: string
+    ) {
+        // Bisogna inizializzare l'errore passandogli il messaggio al costruttore della classe base Error.
+        super(message);
+        this.name = "AppError"; // best practice: sovrascrivere nome con uno più espressivo, di default è "Error".
+    }
+}
---
+++ backend/src/routes/AuthRoutes.ts
@@ -0,0 +1,18 @@
+import Router from "express";
+import { Response, Request } from "express";
+// import { AuthController } from "../controllers/AuthController";
+import { validateLogin } from "../middlewares/AuthMiddleware";
+
+// Istanziamo il router e il controller per gestire le rotte di autenticazione.
+export const authRouter = Router();
+// const authController = new AuthController();
+
+/**
+ * Rotta per il login. Viene passato validateLogin per definire la pipeline di validazione delle credenziali.
+ * 1. Riceve le credenziali (email e password)
+ * 2. Esse vengono validate tramite la pipeline definita in validateLogin(checkEmail e checkPassword) da AuthMiddleware 
+ * 3. Genera il token JWT di autenticazione
+ */
+authRouter.post("/login", validateLogin, (req: Request, res: Response) => {
+    authController.login(req, res);
+});
