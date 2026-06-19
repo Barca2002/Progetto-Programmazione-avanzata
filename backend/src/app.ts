334d04fb981637f42ca301597f0da45f6d435068
Operatore∇ <54327323+SpectreCoded@users.noreply.github.com>
2026-06-19 16:40:27 +0200

    Fix porta applicazione e attributi UserModel

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -10,7 +10,7 @@ import { User } from './models/UserModel.js';
 
 dotenv.config();
 
-const PORT = process.env.LISTEN_PORT;
+const PORT = process.env.APP_PORT;
 const db: Sequelize = DatabaseConnection.connect();
 
 // Inizializzazione del model User
--- backend/src/models/UserModel.ts
+++ backend/src/models/UserModel.ts
@@ -11,18 +11,18 @@ export interface UserAllData {
 }
 
 // user_id è autoincrement e is_admin ha un default, is_admin è opzionale e user_id e il timestamp non si possono inserire
-export interface UserCreation extends Omit<Optional<UserAllData, 'is_admin'>, 'user_id' | 'created_at'> {}
+export interface UserCreationData extends Omit<Optional<UserAllData, 'is_admin'>, 'user_id' | 'created_at'> {}
 
 // <Model<UserAllData, UserCreation>>: usa UserAllData per controllare i dati in lettura, e UserCreation per controllare i dati quando creo un nuovo utente (DA SEQUELIZE)
 //Voglio ottenere un User definito come <Model<UserAllData, UserCreation>> perche mi servono entrambi: il primo per leggere una qualsiasi row del db e l'altro quando verrà creato nel db
-export class User extends Model<UserAllData, UserCreation> implements UserAllData{
+export class User extends Model<UserAllData, UserCreationData> implements UserAllData{
   // Per evitare problemi, dichiariamo gli attributi, così non ci sono campi non dichiarati/non presenti.
-  declare user_id: number;
-  declare username: string;
-  declare email: string;
-  declare password: string;
-  declare is_admin: boolean;
-  declare created_at: Date;
+  user_id!: number;
+  username!: string;
+  email!: string;
+  password!: string;
+  is_admin!: boolean;
+  created_at!: Date;
 
   static inizializzaModel(sequelize: Sequelize): typeof User{
     return User.init({
