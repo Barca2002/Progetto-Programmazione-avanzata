6f55581e5bec4d184679c4189d868ab00c956905
Operatore∇ <54327323+SpectreCoded@users.noreply.github.com>
2026-06-23 11:29:18 +0200

    Refactoring dell'AdminDAO, rimossi tutti i try catch

--- backend/src/dao/AdminDAO.ts
+++ backend/src/dao/AdminDAO.ts
@@ -1,8 +1,12 @@
 import { User } from '../models/UserModel.js';
 import { UserCreationData } from '../models/UserModel.js';
-import { AppErrorEnum } from '../utils/StatusMessages.js';
-import { ErrorFactory } from '../factory/ErrorFactory.js';
 import { Transaction } from 'sequelize';
+
+/*
+* I DAO devono solo occuparsi effettuare operazioni basilari (CRUD) con i dati,
+* quindi le eccezioni/errori devono essere gestiti nei service o controllers.
+*/
+
 
 interface IAdminDAO {
   create(data: UserCreationData, t: Transaction): Promise<User>;
@@ -14,62 +18,35 @@ interface IAdminDAO {
 
 export class AdminDAO implements IAdminDAO {
   async create(data: UserCreationData, t: Transaction): Promise<User> {
-    try{
-      return await User.create(data, {transaction: t});
-    } catch (err){
-      throw ErrorFactory.getError(AppErrorEnum.CREATE_ERROR);
-    }
+    return await User.create(data, {transaction: t});
   }
 
   async findById(user_id: number): Promise<User | null> {
-    try {
-      return await User.findByPk(user_id);
-    } catch (err) {
-      throw ErrorFactory.getError(AppErrorEnum.FIND_ERROR);
-    }
+    return await User.findByPk(user_id);
   }
 
   async findAll(): Promise<User[]> {
-    try{
-      return await User.findAll();
-    } catch (err){
-      throw ErrorFactory.getError(AppErrorEnum.FIND_ERROR);
-    }
+    return await User.findAll();
   }
 
   async findByEmail(email: string): Promise<User | null> {
-    try{
-      return await User.findOne({ where: { email: email } });
-    } catch (err) {
-      throw ErrorFactory.getError(AppErrorEnum.EMAIL_NOT_EXIST);
-    }
+    return await User.findOne({ where: { email: email } });
   }
 
   async findByUsername(username: string): Promise<User | null> {
-    try {
-      return await User.findOne({ where: { username: username } });
-    } catch (error) {
-      throw ErrorFactory.getError(AppErrorEnum.USERNAME_NOT_FOUND);
-    }
-      
+    return await User.findOne({ where: { username: username } });      
   }
 
   async update(user_id: number, data: Partial<UserCreationData>, t: Transaction): Promise<User> {
-    try{
-      const [, affectedRows] = await User.update(data, { where: { user_id: user_id }, transaction: t, returning: true });
-      return affectedRows[0]!;
-    } catch (err){
-      throw ErrorFactory.getError(AppErrorEnum.UPDATE_ERROR);
-    }
+    // Il vettore, nella prima posizione, contiene il numero di righe interessate
+    // dalla modifica, nella seconda posizione contiene le righe interessate.
+    const [, affectedRows] = await User.update(data, { where: { user_id: user_id }, transaction: t, returning: true });
+    return affectedRows[0]!;
   }
 
   async delete(user_id: number, t: Transaction): Promise<number> {
-    try{
       // Ritorna il numero di righe interessate dalla delete
-      return await User.destroy({ where: { user_id: user_id }, transaction: t });
-    } catch (err){
-      throw ErrorFactory.getError(AppErrorEnum.DELETE_ERROR);
-    }
+    return await User.destroy({ where: { user_id: user_id }, transaction: t });
     
   }
 }
