b684f19f5a7f61238a928d8fa9a09ae6b3f9356d
Barca2002 <162043511+Barca2002@users.noreply.github.com>
2026-07-04 22:18:51 +0200

    Change geofenceareaService to private access modifier

--- backend/src/controllers/GeofenceareaController.ts
+++ backend/src/controllers/GeofenceareaController.ts
@@ -2,7 +2,7 @@ import { GeofenceareaService } from "../services/GeofenceareaService.js";
 import { GeofenceareaCreationData } from "../models/GeofenceareaModel.js";
 
 export class GeofenceAreaController {
-  public readonly geofenceareaService = new GeofenceareaService();
+  private readonly geofenceareaService = new GeofenceareaService();
 
   /**
    * Funzione che crea una nuova geofence area
